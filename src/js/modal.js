aa29c236333bb8211916cf0b3e5174f20755792d
vryntrk <184443694+vryntrk@users.noreply.github.com>
2025-12-08 23:16:25 +0300

    social media update

--- src/js/modal.js
+++ src/js/modal.js
@@ -5,8 +5,8 @@ const speakersData = [
 		time: "12.00",
 		profession: "İçerik Üreticisi",
 		bio: "2015 yılından beri çeşitli projelerde çeşitli görevler aldı. 2018 yılında film yapımına tam zamanlı olarak uğraşmak için Bilkent Üniversitesi Ekonomi Bölümü'nü bıraktı. Sinema sektöründe çeşitli pozisyonlarda deneyim kazanmasının yanı sıra videography yaptı. Çektiği kısa filmler ödüller ve seçkiler kazandı. Demir, 2025 yazında ise sosyal medya içerik üreticiliği ile yeni bir maceraya yelken açtı.",
-		instagram: "#",
-		x: "#",
+		instagram: "https://www.instagram.com/saidsakipdemir/",
+		yt: "https://www.youtube.com/@SaidSakipDemir",
 		photo: "said_demir.png"
 	},
 	{
@@ -15,8 +15,8 @@ const speakersData = [
 		time: "13.30",
 		profession: "Sanatçı",
 		bio: "7 yaşından beri illüzyon sanatıyla ilgilenen Yılmaz, henüz 16 yaşında ilk uluslarası yarışmasını kazanarak kariyerinde profesyonel bir adım atmıştır. Sonrasında yurt dışında birçok yarışmada boy göstermiş ve sahne deneyimi kazanmıştır. 25 yaşında 18 yıllık sahne geçmişi olan Yılmaz, çeşitli etkinliklerde yer alarak tecrübelerini ve sanatını kitlelerle paylaşmaya devam etmektedir.",
-		instagram: "#",
-		x: "#",
+		instagram: "https://www.instagram.com/batuyilz/",
+		yt: "https://www.youtube.com/@batuylmz",
 		photo: "batuhan_yilmaz.png"
 	},
 	{
@@ -25,8 +25,8 @@ const speakersData = [
 		time: "15.00",
 		profession: "Dijital İçerik Üreticisi",
 		bio: "Bilkent Üniveritesi Bilgisayar Mühendisliği Bölümü'nden mezun olan Aybalık, 2021 yılında hikaye anlatıcılığı tutkusu ile 'Portal' YouTube kanalını hayata geçirmiştir. Sanat, edebiyat ve felsefe disiplinlerine odaklanan videolarında; ele aldığı sanat eserlerini felsefi ve psikolojik kavramlarla harmanlayıp izleyiciye sunmaktadır. Ayrıca, Spotify üzerinden 'Portal ile Yansıma' ismiyle podcast yayıncılığı da yapmaktadır.",
-		instagram: "#",
-		x: "#",
+		instagram: "https://www.instagram.com/canaybalik/",
+		yt: "https://www.youtube.com/@PortalVideo",
 		photo: "can_aybalik.png"
 	},
 	{
@@ -35,8 +35,8 @@ const speakersData = [
 		time: "12.00",
 		profession: "Antrenör & Diyetisyen",
 		bio: "Sosyal medyada 'Kelömer' ismiyle tanınan Ünal; kendi fiziksel dönüşüm serüveninde edindiği disiplin ve tecrübeyi, bilimsel temellerle birleştirerek profesyonel bir metoda dönüştürmüştür. Sadece yöntemleri değil, yöntemler arkasındaki nedenleri de takipçilerine ve danışanlarına aktarmayı ilke edinmiştir. Gelişimi bir sonuçtan ziyade bir süreç olarak ele alır ve bilimi pratikle harmanlayarak dürüst ve gerçekçi yaklaşımıyla insanlara rehberlik eder.",
-		instagram: "#",
-		x: "#",
+		instagram: "https://www.instagram.com/kelomerofc/",
+		yt: "https://www.youtube.com/@Ömersemiünal",
 		photo: "omer_unal.png"
 	},
 	{
@@ -45,8 +45,8 @@ const speakersData = [
 		time: "13.30",
 		profession: "Eğitimci & Dijital İçerik Üreticisi",
 		bio: "Kimyayı sadece bir ders olarak değil bir hikaye olarak anlatmayı hedefleyen eğitimci ve içerik üreticisidir. YKS öğrencilerine yönelik kitaplar, videolar ve sınıf içi motivasyon projeleriyle kısa sürede geniş bir kitleye ulaşmıştır. Eğlenceli ve akılda kalıcı bir öğrenme süreci yaşatmasının yanı sıra gençlere sahne söyleşileri ve motivasyon konuşmalaryla da ilham vermektedir.",
-		instagram: "#",
-		x: "#",
+		instagram: "https://www.instagram.com/gorkemsahinofficiall/",
+		yt: "https://www.youtube.com/@gorkemsahinn",
 		photo: "gorkem_sahin.png"
 	},
 	{
@@ -55,8 +55,8 @@ const speakersData = [
 		time: "15.00",
 		profession: "Kadın Hastalıkları ve Doğum Uzmanı",
 		bio: "Uludağ Üniversitesi Tıp Fakültesi mezunudur. Uzmanlığını tamamladıktan sonra yüksek lisansını 'halk sağlığı', doktorasını ise 'sosyal psikoloji' üzerine Başkent Üniversitesinde yapmıştır. Birçok üniversitede 'jinekolojik kozmetoloji' alanında eğitmen olarak görev almaktadır. Tıbbi birikimini toplumsal farkındalık çalışmalarıyla birleştiren Erdoğan; 2013, 2014, 2017, 2022 ve 2025 yıllarında kitaplar yayımlamıştır. Bunlara ek olarak 'kozmetik jinekoloji' alanında global ölçekte kabul gören 6 akademik kitabın da editörlüğünü üstlenmiştir.",
-		instagram: "#",
-		x: "#",
+		instagram: "https://www.instagram.com/drgokcenerdogan/",
+		yt: "https://www.youtube.com/c/GökçenErdoğan",
 		photo: "gokcen_erdogan.png"
 	}
 ];
@@ -75,7 +75,7 @@ function createSpeakerElement(speaker, index) {
 		</div>
 		<div class="socials">
 			<a href="${speaker.instagram}"><img class="social" src="logos/instagram_icon.svg"></a>
-			<a href="${speaker.x}"><img class="social" src="logos/x_icon.svg"></a>
+			<a href="${speaker.yt}"><img class="social" src="logos/yt_icon.png"></a>
 		</div>
 	`;
 	return div;
@@ -107,7 +107,7 @@ function openModal(index) {
 	document.getElementById('modalDate').textContent = speaker.date + " " + speaker.time;
 	document.getElementById('modalBio').textContent = speaker.bio;
 	document.getElementById('modalInstagram').href = speaker.instagram || '#';
-	document.getElementById('modalX').href = speaker.x || '#';
+	document.getElementById('modalX').href = speaker.yt || '#';
 	document.getElementById('modalOverlay').classList.add('active');
 }
 
